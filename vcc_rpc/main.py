5efdf520c111f85cf184db188abd304b6190ac3e
calvin <calvinchaochao@outlook.com>
2022-12-20 11:58:22 +0800

    ../..

--- vcc_rpc/main.py
+++ vcc_rpc/main.py
@@ -18,12 +18,15 @@ class Echo(protocol.Protocol):
         if data["type"]=="handshake":
             self.do_handshake(data)
         if data["type"]=="respond":
-
+            self.factory.make_respond(data["jobid"],data["data"])
+        if data["type"]=="request":
+            self.factory.make_request(self,data['service'],data['data'],data["jobid"])
     def do_handshake(self,data):
         if data['role']=="client":
             self.role="client"
             self.factory.clients.append(self)
-            self.transport.write(b'{"res":"ok"}')
+            initial_jobid=uuid.uuid4()
+            self.transport.write(json.dumps({"res":"ok","initial_jobid":initial_jobid}))
             return
         if data["role"]=="service":
             self.role="service"
@@ -31,17 +34,22 @@ class Echo(protocol.Protocol):
                 self.factory.services[i]=self
             self.transport.write(b'{"res":"ok"}')
             return
-    def send_request(self,service,data,jobid):
+    def make_request(self,service,data,jobid):
         data={"type":"call","service":service,"data":data,"jobid":jobid}
         self.transport.write(json.dumps(data))
         return
+    def make_respond(self,jobid,data):
+        pass
 class EchoFactory(protocol.Factory):
     services={}
     clients=[]
     promises={}
     def make_request(self,client,service,data,jobid):
-        self.services[service].send_request(service,data,jobid)
-
+        self.services[service].make_request(service,data,jobid)
+        self.promises[jobid]=client
+    def make_respond(self,jobid,data):
+        self.promises[jobid].make_respond(jobid,data)
+        del self.promises[jobid]
     def buildProtocol(self, addr):
         return Echo(self)
 
