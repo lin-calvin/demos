0d21537c098bec7742ac0ce929e7a1cf40f06c79
calvin <calvinchaochao@outlook.com>
2022-12-23 16:37:49 +0800

    ../..

--- vcc_rpc/main.py
+++ vcc_rpc/main.py
@@ -1,67 +1,74 @@
 import json
 import uuid
-import logging
 from twisted.internet import protocol, reactor, endpoints
 
-logging.basicConfig(level=logging.DEBUG)
 
 class RpcProtocol(protocol.Protocol):
     def __init__(self, factory):
-        logging.debug(1)
-        self.factory = factory
-
-    def send(self, obj):
-        self.transport.write(bytes(json.dumps(obj), "UTF8"))
+        print(1)
+        self.factory: EchoFactory = factory
 
     def connectionLost(self,res):
         if self.role=="client":
             self.factory.clients.remove(self)
+        return
     def dataReceived(self, data):
         try:
             data = json.loads(data)
-            logging.debug(data)
+            print(data)
         except json.JSONDecodeError:
-            self.send({"res":"error","error":"not json"})
+            self.transport.write(b'{"res":"error","error":"not json"}')
             return
         if "res" in data:
             return
         if data["type"] == "handshake":
             self.do_handshake(data)
-            logging.debug(self.factory.services)
-        elif self not in self.factory.clients:
-            self.send({"res":"error","error":"invalid request"})
-        elif data["type"] == "respond":
+            print(self.factory.services)
+        if self.role!="service" and self not in self.factory.clients:
+            self.transport.write(b'{"res":"error","error":"invalid request"}')
+            return
+        if data["type"] == "respond":
             self.factory.make_respond(data["jobid"], data["data"])
-            ret = {"res": "ok", "next_jobid": str(uuid.uuid4())}
-            self.send(ret)
-        elif data["type"] == "request":
-            self.factory.make_request(
-                self, data["service"], data["data"], data["jobid"]
-            )
 
+        if data["type"] == "request":
+            try:
+                self.factory.make_request(
+                    self, data["service"], data["data"], data["jobid"]
+                )
+                ret="ok"
+            except KeyError:
+                ret="err"
+
+            ret = {"res": ret, "next_jobid": str(uuid.uuid4())}
+            self.transport.write(bytes(json.dumps(ret), "UTF8"))
     def do_handshake(self, data):
         self.role=data["role"]
         if data["role"] == "client":
             self.role = "client"
             self.factory.clients.append(self)
             initial_jobid = uuid.uuid4()
-            self.send({"res": "ok", "initial_jobid": str(initial_jobid)})
-                    
+            self.transport.write(
+                bytes(
+                    json.dumps({"res": "ok", "initial_jobid": str(initial_jobid)}),
+                    "UTF8",
+                )
+            )
             return
         if data["role"] == "service":
             self.role = "service"
-            for i in data["services"]:
+            for i in (serv := data["services"]):
                 self.factory.services[i] = self
-            self.send({"res":"ok"})
+            self.transport.write(b'{"res":"ok"}')
             return
 
     def make_request(self, service, data, jobid):
         data = {"type": "call", "service": service, "data": data, "jobid": jobid}
-        self.send(data)
+        self.transport.write(bytes(json.dumps(data), "UTF8"))
+        return
 
     def make_respond(self, jobid, data):
         data = {"type": "resp", "data": data, "jobid": jobid}
-        self.send(data)
+        self.transport.write(bytes(json.dumps(data), "UTF8"))
 
 
 class RpcServer(protocol.Factory):
@@ -70,7 +77,10 @@ class RpcServer(protocol.Factory):
     promises = {}
 
     def make_request(self, client, service, data, jobid):
-        self.services[service].make_request(service, data, jobid)
+        try:
+            self.services[service].make_request(service, data, jobid)
+        except KeyError:
+            raise KeyError("Not such service")
         self.promises[jobid] = client
 
     def make_respond(self, jobid, data):
@@ -81,5 +91,5 @@ class RpcServer(protocol.Factory):
         return RpcProtocol(self)
 
 
-endpoints.serverFromString(reactor, "tcp:274").listen(RpcServer())
+endpoints.serverFromString(reactor, "tcp:1234").listen(RpcServer())
 reactor.run()
