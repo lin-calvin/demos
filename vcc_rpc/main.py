8fb6315a92d7f89722267c9499fbfdd23c783976
3swordman <yyuxiaoran@qq.com>
2022-12-24 12:13:46 +0800

    refactor: make the code nicer

--- vcc_rpc/main.py
+++ vcc_rpc/main.py
@@ -1,79 +1,63 @@
 import json
 import uuid
-from twisted.internet import protocol, reactor, endpoints
+import logging
 
+from twisted.internet import protocol, reactor, endpoints
 
 class RpcProtocol(protocol.Protocol):
     def __init__(self, factory):
-        print(1)
-        self.factory: EchoFactory = factory
-
-    def connectionLost(self,res):
-        if self.role=="client":
-            self.factory.clients.remove(self)
-        return
+        logging.debug(1)
+        self.factory: RpcServer = factory
+    
     def dataReceived(self, data):
         try:
             data = json.loads(data)
-            print(data)
+            logging.debug(data)
         except json.JSONDecodeError:
-            self.transport.write(b'{"res":"error","error":"not json"}')
+            self.send({"res": "error", "error": "not json"})
             return
         if "res" in data:
             return
-        if data["type"] == "handshake":
-            self.do_handshake(data)
-            print(self.factory.services)
-        if self.role!="service" and self not in self.factory.clients:
-            self.transport.write(b'{"res":"error","error":"invalid request"}')
-            return
-        if data["type"] == "respond":
-            self.factory.make_respond(data["jobid"], data["data"])
+        match data["type"]:
+            case "handshake":
+                self.do_handshake(data)
+                logging.debug(self.factory.services)
+            case "respond" if self.role == "service":
+                self.factory.make_respond(data["jobid"], data["data"])
+            case "request" if self.role == "client":
+                try:
+                    self.factory.make_request(
+                        self, data["service"], data["data"], data["jobid"]
+                    )
+                    ret = "ok"
+                except KeyError:
+                    ret = "err"
+                self.send({"res": ret, "next_jobid": str(uuid.uuid4())})
+            case _:
+                self.send({"res": "error", "error": "invalid request"})
 
-        if data["type"] == "request":
-            try:
-                self.factory.make_request(
-                    self, data["service"], data["data"], data["jobid"]
-                )
-                ret="ok"
-            except KeyError:
-                ret="err"
-
-            ret = {"res": ret, "next_jobid": str(uuid.uuid4())}
-            self.transport.write(bytes(json.dumps(ret), "UTF8"))
+    def send(self, data):
+        self.transport.write(json.dumps(data).encode())
+    
     def do_handshake(self, data):
-        self.role=data["role"]
+        self.role = data["role"]
         if data["role"] == "client":
-            self.role = "client"
-            self.factory.clients.append(self)
             initial_jobid = uuid.uuid4()
-            self.transport.write(
-                bytes(
-                    json.dumps({"res": "ok", "initial_jobid": str(initial_jobid)}),
-                    "UTF8",
-                )
-            )
-            return
-        if data["role"] == "service":
-            self.role = "service"
-            for i in (serv := data["services"]):
+            self.send({"res": "ok", "initial_jobid": str(initial_jobid)})
+        elif data["role"] == "service":
+            for i in data["services"]:
                 self.factory.services[i] = self
-            self.transport.write(b'{"res":"ok"}')
-            return
+            self.send({"res": "ok"})
 
     def make_request(self, service, data, jobid):
-        data = {"type": "call", "service": service, "data": data, "jobid": jobid}
-        self.transport.write(bytes(json.dumps(data), "UTF8"))
-        return
+        self.send({"type": "call", "service": service, "data": data, "jobid": jobid})
 
     def make_respond(self, jobid, data):
-        data = {"type": "resp", "data": data, "jobid": jobid}
-        self.transport.write(bytes(json.dumps(data), "UTF8"))
+        self.send({"type": "resp", "data": data, "jobid": jobid})
 
 
 class RpcServer(protocol.Factory):
     services = {}
-    clients = []
     promises = {}
 
     def make_request(self, client, service, data, jobid):
